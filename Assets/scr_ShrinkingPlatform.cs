e60541f2b4d7b69743f8941fe6b66c6c3c19cad2
Sam <samcpollock@gmail.com>
2021-12-17 15:24:04 -0500

    Added shrinking functionality to platform

--- Assets/scr_ShrinkingPlatform.cs
+++ Assets/scr_ShrinkingPlatform.cs
@@ -4,11 +4,50 @@ using UnityEngine;
 
 public class scr_ShrinkingPlatform : MonoBehaviour
 {
+
+    public float rateOfShrink;
+    public float waitTimeBeforeGrowing; 
+
+    private Animator animator;
+    //private GameObject centerOfPlatform; 
+
+    private bool isShrinking = false;
+    private float timeSincePlayerContact = 0;
+
+
+    private void Start()
+    {
+        animator = GetComponentInParent<Animator>();
+        //centerOfPlatform = gameObject.transform.GetChild(0).gameObject;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (isShrinking)
+        {
+            gameObject.transform.localScale *= (rateOfShrink);
+        }
+        else
+        {
+            timeSincePlayerContact += Time.deltaTime;
+
+            if (timeSincePlayerContact >= waitTimeBeforeGrowing && gameObject.transform.localScale.x < 1f)
+            {
+                gameObject.transform.localScale *= (2 - rateOfShrink);
+            }
+        }
+
+      
+        
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("PLAYER HAS LANDED ON PLATORM");
+            StartShrinking();
         }
     }
 
@@ -16,7 +55,20 @@ public class scr_ShrinkingPlatform : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("PLAYER HAS LEFT THE PLATORM");
+            StopShrinking();
         }
+    }
+
+
+    private void StartShrinking()
+    {
+        isShrinking = true;
+    }
+
+    private void StopShrinking()
+    {
+        Debug.Log("PLAYER HAS LEFT THE PLATORM");
+        isShrinking = false;
+        timeSincePlayerContact = 0;
     }
 }
